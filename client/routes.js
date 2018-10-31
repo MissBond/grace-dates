bb31414962a96af53a62d76b4f2e255ec824d41a
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2018-10-31 14:13:06 -0400

    connected all celebs and single celebs component, tested add form, blocked on update form

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, AllCelebrities} from './components'
+import {Login, Signup, UserHome, AllCelebrities, SingleCelebrity} from './components'
 import {me} from './store'
 
 /**
@@ -21,7 +21,8 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/celebrities" component={AllCelebrities} />
+        <Route exact path="/celebrities" component={AllCelebrities} />
+        <Route path="/celebrities/:celebrityId" component={SingleCelebrity} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
--- client/store/index.js
+++ client/store/index.js
@@ -4,15 +4,15 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import celebrities from './celebrities'
+import oneCelebrity from './single-celebrity'
 
-const reducer = combineReducers({user, celebrities})
+const reducer = combineReducers({user, celebrities, oneCelebrity})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 const store = createStore(reducer, middleware)
 
-console.log('THIS IS THE STORE FROM /STORE/INDEX.JS', store)
-
 export default store
 export * from './user'
-// export * from './celebrities'
+export * from './celebrities'
+export * from './single-celebrity'
