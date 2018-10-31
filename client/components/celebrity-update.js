3cedcab85f5e03fb273ac9674297af35f3a584f3
Angela Roberts <angelajeanroberts@gmail.com>
2018-10-30 22:05:50 -0400

    Created update form for single celebrity and set/update/remove thunks for single celebrity

---
+++ client/components/celebrity-update.js
@@ -0,0 +1,118 @@
+import React, {Component} from 'react'
+import {updateSelectedCelebrity} from '../store'
+import {connect} from 'react-redux'
+
+class CelebrityUpdate extends Component {
+  constructor(props) {
+    super(props)
+    this.state = this.props.selectedCelebrity
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.updateCelebrity(this.state, this.props.selectedCelebrityId)
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="imageUrl">Image Link:</label>
+          <input
+            type="text"
+            name="imageUrl"
+            value={this.state.imageUrl}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="occupation">Occupation:</label>
+          <input
+            type="text"
+            name="occupation"
+            value={this.state.occupation}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="gender">Gender:</label>
+          <input
+            type="text"
+            name="gender"
+            value={this.state.gender}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="netWorthMillions">Net Worth ($millions):</label>
+          <input
+            type="text"
+            name="netWorthMillions"
+            value={this.state.netWorthMillions}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description:</label>
+          <input
+            type="text"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    selectedCelebrity: {
+      firstName: state.selectedCelebrity.firstName,
+      lastName: state.selectedCelebrity.lastName,
+      imageUrl: state.selectedCelebrity.email,
+      occupation: state.selectedCelebrity.imageUrl,
+      gender: state.selectedCelebrity.gender,
+      netWorthMillions: state.selectedCelebrity.netWorthMillions,
+      description: state.selectedCelebrity.description
+    },
+    selectedCelebrityId: state.selectedCelebrity.id
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    updateCelebrity: (celebrityToUpdate, celebrityId) => {
+      dispatch(updateSelectedCelebrity(celebrityToUpdate, celebrityId))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CelebrityUpdate)
---
+++ client/store/single-celebrity.js
@@ -0,0 +1,63 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GET_CELEBRITY = 'GET_CELEBRITY'
+const UPDATE_CELEBRITY = 'UPDATE_CELEBRITY'
+const REMOVE_CELEBRITY = 'REMOVE_USER'
+
+/**
+ * INITIAL STATE
+ */
+const defaultCelebrity = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getCelebrity = celebrity => ({type: GET_CELEBRITY, celebrity})
+const updateCelebrity = updatedCelebrity => ({
+  type: UPDATE_CELEBRITY,
+  updatedCelebrity
+})
+const removeCelebrity = celebrityId => ({type: REMOVE_CELEBRITY, celebrityId})
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchCelebrity = celebrityId => {
+  return async dispatch => {
+    const {data} = await axios.get(`/api/celebrities/${celebrityId}`)
+    dispatch(getCelebrity(data))
+  }
+}
+
+export const updateSelectedCelebrity = (celebrity, celebrityId) => {
+    return async dispatch => {
+        const updatedCelebrity = await axios.put(`/api/celebrities/${celebrityId}`, celebrity)
+        dispatch(updateCelebrity(updatedCelebrity.data))
+    }
+}
+
+export const removeSelectedCelebrity = celebrityId => {
+    return async dispatch => {
+        await axios.delete(`/api/celebrities/${celebrityId}`)
+        dispatch(removeCelebrity(celebrityId))
+    }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultCelebrity, action) {
+  switch (action.type) {
+    case GET_CELEBRITY:
+      return action.celebrity
+    case UPDATE_CELEBRITY:
+        return action.updatedCelebrity //this is a placeholder, we will need to update the list of celebrities in the store (i.e. update information by matching on id)
+    case REMOVE_CELEBRITY:
+      return defaultCelebrity //this is a placeholder, we will need to update the list of celebrities in the store (i.e. delete celebrity by matching on id)
+    default:
+      return state
+  }
+}
