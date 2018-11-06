7236c5258f8e23f82cb71f215b8a1a66f46767f4
Danielle Bond <dbond603@gmail.com>
2018-11-06 12:05:12 -0500

    third commit

--- client/components/checkoutConfirmation.js
+++ client/components/checkoutConfirmation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const CheckoutConfirmation = () => {
+    const orderNumber = (n) => n+ 100000;
     return (
       <div>
         <h1>Your purchase is complete!</h1>
         <h2>Thank you for your order!</h2>
-        <p>Your order confirmation number is {Math.floor(Math.random() * 100000) + 1 }</p>
+        <p>Your order confirmation number is {orderNumber()}</p>
         <h3>Hope your as excited as we are for your celeb date!</h3>
       </div>
     )
